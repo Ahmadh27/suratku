6e4fcae789e9be8ea89ec03bfa3db8a79e0a495d
ahmadh27 <ahmita2728@gmail.com>
2024-05-09 09:18:12 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// // 
+// const surat = document.getElementById('surat')
+// // surat.style.fontSize = '50px'
+
+// const coba = document.createElement("h6")
+// coba.innerHTML = "inda riska"
+
+// surat.append(coba)
+// console.log(coba)
+
+// // getElementId
+// // getElementClassName
+// // َquerySelector(".right .nav h3")
+// // َquerySelectorAll(".right .nav h3")
+
+// // 
+// surat.addEventListener("click", ()=>{
+//     surat.style.color = "red"
+//     alert("hgfr")
+// })
+
+// const data = [
+//     ["Inda", 'Surat Cinta', '20 April 2024'],
+//     ["Riska", 'Surat Cinta', '20 April 2024'],
+//     ["Uswatun", 'Surat Cinta', '20 April 2024'],
+//     ["Nisa", 'Surat Cinta', '20 April 2024'],
+// ]
+
+// const box = document.getElementById("box")
+
+// data.map((e)=>{
+//     box.innerHTML += `<div class="menu1">
+//     <div class="User">
+//         <img src="img/Ellipse.png" alt="">
+//         <p>${e[0]}</p>
+//     </div>
+//     <img src="img/word.png" alt="">
+//     <h3>${e[1]}</h3>
+//     <h4>${e[2]}</h4>
+// </div>`
+// })
